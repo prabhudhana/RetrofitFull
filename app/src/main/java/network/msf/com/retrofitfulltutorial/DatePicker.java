5bd409e6c0e2dc269e66a762f948112a3fb19499
Dhanaprabhu <dhanaprabhu.inxsasia.com>
2018-12-31 15:38:39 +0530

    added calendar

---
+++ app/src/main/java/network/msf/com/retrofitfulltutorial/DatePicker.java
@@ -0,0 +1,71 @@
+package network.msf.com.retrofitfulltutorial;
+
+import android.app.DatePickerDialog;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DatePicker extends AppCompatActivity {
+
+    EditText editText;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+        calendar = Calendar.getInstance();
+        editText = (EditText) findViewById(R.id.Birthday);
+
+        final DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(android.widget.DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR, i);
+                calendar.set(Calendar.MONTH, i1);
+                calendar.set(Calendar.DAY_OF_MONTH, i2);
+                updateLabel();
+            }
+
+
+        };
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(DatePicker.this, onDateSetListener, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+
+                calendar.add(Calendar.DAY_OF_MONTH, -7);
+                Date result = calendar.getTime();
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.getDatePicker().setMinDate(result.getTime());
+                //Disable past days
+                //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+
+                datePickerDialog.show();
+                /*new DatePickerDialog(DatePicker.this, onDateSetListener, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();*/
+            }
+        });
+
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/YY"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editText.setText(sdf.format(calendar.getTime()));
+    }
+}
